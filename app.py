f2c63d58b12fe45fd88998551f09d10d04e67df4
dohyeon kim <xingchen8837@gmail.com>
2026-01-19 20:23:18 +0900

    CORS 에러 수정

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 from utils.profiler import trace, profiler
 
 app = Flask(__name__)
+CORS(app) # 모든 origin에 대해 CORS 허용
 
 @app.after_request
 def after_each_request(response):
